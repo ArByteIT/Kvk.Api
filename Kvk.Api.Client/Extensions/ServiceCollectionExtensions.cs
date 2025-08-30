def2a4f909d08130b792c0a9771d9eca920bfb9c
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2025-08-30 19:23:13 +0200

    Refactor Kvk API client registration methods

--- Kvk.Api.Client/Extensions/ServiceCollectionExtensions.cs
+++ Kvk.Api.Client/Extensions/ServiceCollectionExtensions.cs
@@ -12,11 +12,11 @@ namespace Kvk.Api.Client.Extensions;
 public static class ServiceCollectionExtensions
 {
     /// <summary>
-    /// Adds the Kvk API client to the service collection.
+    /// Adds the Kvk API V1 client to the service collection.
     /// </summary>
     /// <param name="services">The service collection.</param>
     /// <returns>The updated service collection.</returns>
-    public static IServiceCollection AddKvkApiClient(this IServiceCollection services, IConfiguration configuration)
+    public static IHttpClientBuilder AddKvkApiV1Client(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<KvkOptions>(configuration.GetRequiredSection(KvkOptions.SectionName));
 
@@ -27,7 +27,7 @@ public static class ServiceCollectionExtensions
 
         // V1 Client
         services.AddTransient<IKvkApiV1Client, KvkApiV1Client>();
-        services.AddHttpClient(KvkOptions.HttpV1ClientName, (provider, client) =>
+        return services.AddHttpClient(KvkOptions.HttpV1ClientName, (provider, client) =>
         {
             var options = provider.GetRequiredService<IOptions<KvkOptions>>().Value;
 
@@ -36,10 +36,25 @@ public static class ServiceCollectionExtensions
             client.DefaultRequestHeaders.Add("apikey", options.ApiKey);
         })
             .AddHttpMessageHandler<KvkApiErrorHandler>();
+    }
+
+    /// <summary>
+    /// Adds the Kvk API V2 client to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IHttpClientBuilder AddKvkApiV2Client(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<KvkOptions>(configuration.GetRequiredSection(KvkOptions.SectionName));
+
+        services.AddTransient<KvkApiErrorHandler>();
+
+        var productName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name ?? "KvkApiClient";
+        var productVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
 
         // V2 Client
         services.AddTransient<IKvkApiV2Client, KvkApiV2Client>();
-        services.AddHttpClient(KvkOptions.HttpV2ClientName, (provider, client) =>
+        return services.AddHttpClient(KvkOptions.HttpV2ClientName, (provider, client) =>
         {
             var options = provider.GetRequiredService<IOptions<KvkOptions>>().Value;
 
@@ -48,7 +63,5 @@ public static class ServiceCollectionExtensions
             client.DefaultRequestHeaders.Add("apikey", options.ApiKey);
         })
             .AddHttpMessageHandler<KvkApiErrorHandler>();
-
-        return services;
     }
 }
